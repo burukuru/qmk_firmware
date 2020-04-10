2e95c9578757a7c471c6c1d53ec7e5b0e0570769
Thanh C Pham <burukuru@gmail.com>
2020-04-10 22:35:46 +0100

    Add dz60 keymap

--- keyboards/dz60/keymaps/iso_split-spacebar/keymap.c
+++ keyboards/dz60/keymaps/iso_split-spacebar/keymap.c
@@ -8,7 +8,7 @@
 // https://github.com/AGausmann/qmk_firmware/blob/agausmann-v3.x/keyboards/nyquist/keymaps/agausmann/keymap.c
 
 #include QMK_KEYBOARD_H
-
+#include "keymap_uk.h"
 /* 
 * Each layer gets a name for readability.
 * The underscores don't mean anything - you can
@@ -69,11 +69,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
    [BL] = LAYOUT_60_iso_split(
 	//  1          2          3          4          5          6          7          8          9          10         11         12         13         14
-	    KC_GRV,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSPC,
-        KC_TAB ,   KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_LBRC,   KC_RBRC,
-        KC_CAPS,   KC_A,      KC_S,      KC_D,      KC_F,      KC_G,      KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,   KC_NUHS,   KC_ENT,
-        KC_LSFT,   KC_NUBS,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   KC_RSFT,
-        KC_LCTL,   KC_LGUI,   KC_LALT,   KC_SPC,    MO(FL),    KC_SPC,    KC_RALT,   KC_RGUI,   TO(NL),    KC_RCTL),
+	    UK_ESC,    UK_1,      UK_2,      UK_3,      UK_4,      UK_5,      UK_6,      UK_7,      UK_8,      UK_9,      UK_0,      UK_MINS,   UK_EQL,    UK_INS,
+        UK_TAB ,   UK_Q,      UK_W,      UK_E,      UK_R,      UK_T,      UK_Y,      UK_U,      UK_I,      UK_O,      UK_P,      UK_LBRC,   UK_RBRC,
+        UK_LCTL,   UK_A,      UK_S,      UK_D,      UK_F,      UK_G,      UK_H,      UK_J,      UK_K,      UK_L,      UK_SCLN,   UK_QUOT,   UK_HASH,   UK_ENT,
+        KC_LSHIFT,   UK_BSLS,   UK_Z,      UK_X,      UK_C,      UK_V,      UK_B,      UK_N,      UK_M,      UK_COMM,   UK_DOT,    UK_SLSH,   UK_RSFT,
+        UK_LCTL,   UK_LGUI,   UK_LALT,   UK_SPC,    MO(FL),    UK_BSPC,    _______,   KC_MENU,   TO(NL),    UK_RCTL),
 
   /* Keymap FL: Function Layer
    *
@@ -91,11 +91,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
    [FL] = LAYOUT_60_iso_split(
 	//  1          2          3          4          5          6          7          8          9          10         11         12         13         14
-	    KC_ESC,    KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_DEL,
-        _______ ,  _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   KC_PSCR,   _______,   _______,
-        _______,   KC_MS_LEFT,KC_MS_DOWN,KC_MS_UP,  KC_MS_RIGHT,_______,  _______,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  _______,   _______,   _______,
-        _______,   _______,   _______,   _______,   _______,   KC_MS_BTN1,KC_MS_BTN2,_______,   KC_HOME,   KC_PGDOWN, KC_PGUP,   KC_END,    _______,
-        _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______),
+	    UK_GRV,    UK_F1,     UK_F2,     UK_F3,     UK_F4,     UK_F5,     UK_F6,     UK_F7,     UK_F8,     UK_F9,     UK_F10,    UK_F11,    UK_F12,    _______,
+        _______ ,  _______,   _______,   _______,   _______,   _______,   UK_HOME,   UK_PGDN,     UK_PGUP,   UK_END,     UK_PSCR,   _______,   _______,
+        UK_LCTL,   _______,   _______,   _______,   _______,   _______,  UK_LEFT,   UK_DOWN,       UK_UP,     UK_RGHT,  _______,   _______,   _______,   _______,
+        _______,   _______,   _______,   _______,   _______,   _______,  _______,  _______,   UK_HOME,   UK_PGDN, UK_PGUP,   UK_END,    _______,
+        _______,   _______,   _______,   _______,   _______,   UK_DEL,    _______,   _______,   _______,   _______),
 
   /* Keymap NL: Numpad Layer
    *
