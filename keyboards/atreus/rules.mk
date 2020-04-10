1e376db41dda8640bcb8e0eb731d54b445bbcc8f
Thanh C Pham <burukuru@gmail.com>
2020-04-10 22:34:43 +0100

    Add atreus keymap.

--- keyboards/atreus/rules.mk
+++ keyboards/atreus/rules.mk
@@ -19,4 +19,4 @@ AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no 		# Enable support for HD44780 based LCDs
 
-DEFAULT_FOLDER = atreus/astar
+DEFAULT_FOLDER = atreus/promicro
